32595583e88a6db9bb67f192339dd366a380e072
Manuel A. Rodriguez <manuel@rdrsss.io>
2017-05-09 20:57:55 +0000

    stub in test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,9 @@
 
 #include "gtest/gtest.h"
 
+// TODO :: write usit tests
+#include "linkedlist.h"
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
